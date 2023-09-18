b2785e7bec057155e53f9c28a36a25176efc0ddb
zer0guz <zer0guz>
2023-09-18 14:14:43 +0200

    first

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cat bashrc.additions >> ~/.bashrc
-
 cp ./.gitmessage ~
 cp ./.gitconfig ~
 
-# powerline fonts for zsh agnoster theme
+# powerline fonts for zsh
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
@@ -17,18 +15,7 @@ zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
 
-########################################################################################################################
-#### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
-#### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
-# save current zshrc
-mv ~/.zshrc ~/.zshrc.bak
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && /usr/sbin/locale-gen
 
-sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -t agnoster
-
-# remove newly created zshrc
-rm -f ~/.zshrc
-# restore saved zshrc
-mv ~/.zshrc.bak ~/.zshrc
-# update theme
-sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc 
+cp tmux.conf /root/.tmux.conf 
+cp pwninit-template.py /root/.config/pwninit-template.py
